140278129050c0dc10e17a84e925e21d97332f7d
takyEhab <tkyehab@gmail.com>
2026-09-22 20:21:07 +0300

    supporting mutiple langauges + adding axios and egp format

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -9,3 +9,13 @@ api.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (r) => r,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem("drivo_token");
+    }
+    return Promise.reject(err);
+  },
+);
---
+++ client/src/lib/format.js
@@ -0,0 +1,5 @@
+export function egp(amount) {
+  return new Intl.NumberFormat('en-EG', {
+    style: 'currency', currency: 'EGP', maximumFractionDigits: 0,
+  }).format(Number(amount));
+}
